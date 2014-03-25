c58190be26439011d22a2f25657465eed243492f
countdown <bz0915@gmail.com>
2014-03-25 03:06:11 -0700

    add multithread copy files
    add multithread copy files

--- CommonLibrary/FileManager.cs
+++ CommonLibrary/FileManager.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -43,6 +44,162 @@ namespace CommonLibrary
                 }
             }
         }
+
+        public void CopyFiles(string source, string destination, bool overwriteExisting = true, bool logEachFile = true)
+        {
+            // Validate the input arguments.
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentException("Argument cannot be null or empty.", "source");
+            }
+
+            if (string.IsNullOrEmpty(destination))
+            {
+                throw new ArgumentException("Argument cannot be null or empty.", "destination");
+            }
+
+            // Make sure that both the source and destination are full paths.
+            string fullDestination = destination;
+            string fullSource = source;
+
+            if (!Path.IsPathRooted(fullDestination))
+            {
+                fullDestination = Path.Combine(Environment.CurrentDirectory, fullDestination);
+            }
+
+            if (!Path.IsPathRooted(fullSource))
+            {
+                fullSource = Path.Combine(Environment.CurrentDirectory, fullSource);
+            }
+
+            if (string.IsNullOrEmpty(Path.GetExtension(fullDestination)) && !fullDestination.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                fullDestination = string.Format("{0}{1}", fullDestination, Path.DirectorySeparatorChar);
+            }
+
+            if (string.IsNullOrEmpty(Path.GetExtension(fullSource)) && !fullSource.EndsWith("*"))
+            {
+                // The source is a directory, make sure it has a trailing slash
+                if (!fullSource.EndsWith(Path.DirectorySeparatorChar.ToString()))
+                {
+                    fullSource = fullSource + Path.DirectorySeparatorChar;
+                }
+            }
+
+            string sourceDir = Path.GetDirectoryName(fullSource);
+            string sourceFile = Path.GetFileName(fullSource);
+            string destinationDir = Path.GetDirectoryName(fullDestination);
+
+            // Search for all files we need to copy.
+            //LogManager.LogMessage("Searching for files to copy.", LogMessageLevel.Info);
+            if (string.IsNullOrEmpty(sourceFile))
+            {
+                sourceFile = "*";
+            }
+
+            string[] files = null;
+            if (sourceFile.StartsWith("*"))
+            {
+                files = Directory.GetFiles(sourceDir, sourceFile, SearchOption.AllDirectories);
+            }
+            else
+            {
+                files = Directory.GetFiles(sourceDir, sourceFile, SearchOption.TopDirectoryOnly);
+            }
+
+            //LogManager.LogMessage("{0} files found.", LogMessageLevel.Info, files.Length);
+
+            // If no files were found, simply return.
+            if (files.Length == 0)
+            {
+                //return;
+                throw new InvalidOperationException(String.Format("The source {0} could not be found", source));
+            }
+
+            if (files.Length > 1 && !string.IsNullOrEmpty(Path.GetExtension(fullDestination)))
+            {
+                throw new InvalidOperationException("The destination you specified is a file when the source contains multiple files.");
+            }
+
+            // Create a queue of files to copy.
+            Queue<CopyFileCommand> m_fileCopyQueue;
+            List<CopyFileCommand> m_fileCopiedList;
+            m_fileCopyQueue = new Queue<CopyFileCommand>(files.Length);
+            m_fileCopiedList = new List<CopyFileCommand>(files.Length);
+            for (int i = 0; i < files.Length; ++i)
+            {
+                string destinationPath = string.Concat(destinationDir, files[i].Substring(sourceDir.Length));
+
+                lock (m_fileCopyQueue)
+                {
+                    m_fileCopyQueue.Enqueue(new CopyFileCommand(files[i], destinationPath, overwriteExisting));
+                }
+            }
+
+            // Launch the threads to perform the file copy.
+            Thread copyThread1 = new Thread(new ThreadStart(MultiThreadedFileCopy));
+            Thread copyThread2 = new Thread(new ThreadStart(MultiThreadedFileCopy));
+            Thread copyThread3 = new Thread(new ThreadStart(MultiThreadedFileCopy));
+            Thread copyThread4 = new Thread(new ThreadStart(MultiThreadedFileCopy));
+            Thread copyThread5 = new Thread(new ThreadStart(MultiThreadedFileCopy));
+
+            copyThread1.Start();
+            copyThread2.Start();
+            copyThread3.Start();
+            copyThread4.Start();
+            copyThread5.Start();
+
+            while (copyThread1.IsAlive || copyThread2.IsAlive || copyThread3.IsAlive
+                || copyThread4.IsAlive || copyThread5.IsAlive)
+            {
+                System.Threading.Thread.Sleep(500);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            //the verbosity here causes long delays in loading the results into the UI when there are tons of files...
+            //users will get the old behavior by default
+            if (logEachFile)
+            {
+                for (int i = 0; i < m_fileCopiedList.Count; ++i)
+                {
+                    sb.AppendLine(string.Format("copied {0} --> {1}", m_fileCopiedList[i].Source, m_fileCopiedList[i].Destination));
+                }
+            }
+            LogManager.LogMessage(sb.ToString(), LogMessageLevel.Info);
+            LogManager.LogMessage("{0} of {1} file(s) copied successfully.", LogMessageLevel.Info, m_fileCopiedList.Count, files.Length);
+
+            if (m_fileCopyQueue.Count != 0 || m_fileCopiedList.Count != files.Length)
+            {
+                throw new Exception("An unexpected error has occurred during the file copy process.");
+            }
+
+        }
+
+        /// <summary>
+        /// Copies the list of files in the sources to the destination.
+        /// </summary>
+        /// <param name="sources">Either a file or directory.  Can contain wildcards.</param>
+        /// <param name="destination">Either a file or directory.</param>
+        public void CopyFiles(string[] sources, string destination)
+        {
+            // Validate the input arguments.
+            if (sources == null)
+            {
+                throw new ArgumentNullException("Argument cannot be null.", "sources");
+            }
+
+            if (string.IsNullOrEmpty(destination))
+            {
+                throw new ArgumentException("Argument cannot be null or empty.", "destination");
+            }
+
+            foreach (string source in sources)
+            {
+                CopyFiles(source, destination);
+            }
+        }
+
     }
 
     public static class ImageManager
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommonLibrary;
+using System.IO;
+using CommonLibrary;
 
 namespace ConsoleApplication1
 {
@@ -11,7 +13,25 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            CommandManager.ExecuteCommand(@"D:\code\batch\test.bat");
+
+            string projectName = args[0];
+            //@"D:\school\tfs\Src\TFS\common\tools\TFSAutoStarterService\TFSAutoStarterStub\TFSAutoStarterStub.csproj";
+            if (!File.Exists(projectName))
+                throw new Exception("Project not exists!");
+
+            string msbuildPath = string.Empty;
+            if (File.Exists(@"D:\SPITests\binaries\suitebin\msbuild.exe"))
+                msbuildPath = @"D:\SPITests\binaries\suitebin\msbuild.exe";
+            else if (File.Exists(@"D:\Program Files\MSBuild\12.0\Bin\msbuild.exe"))
+                msbuildPath = @"D:\Program Files\MSBuild\12.0\Bin\msbuild.exe";
+            else if (File.Exists(@"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"))
+                msbuildPath = @"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe";
+            else if (File.Exists(@"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"))
+                msbuildPath = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe";
+            if (msbuildPath == string.Empty)
+                throw new Exception("msbuild.exe could not found!");
+
+            CommandManager.ExecuteCommand(msbuildPath, projectName);
         }
     }
 }
